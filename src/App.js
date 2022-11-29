17c868c67eebb438ffce264e0a83752b57408941
Rafael Musse <rafaelmusse@MacBook-Air-de-Rafael.local>
2022-11-29 17:31:23 -0300

    requisito 2 ao 6 - Login terminados

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Meals from './pages/Meals';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/meals" component={ Meals } />
     </Switch>
   );
 }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { renderWithRouter } from './RenderWithL';
 
 test('Farewell, front-end', () => {
   // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+  renderWithRouter(<App />);
+  expect(true).toBe(true);
 });
