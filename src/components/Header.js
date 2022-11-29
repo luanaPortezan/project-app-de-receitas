17a4569c1d5289fe658d2724a62d10f395a3eb5c
Luana Portezan <luanaportezan@gmail.com>
2022-11-29 18:18:37 -0300

    (req-8) redicionamento da tela ao clicar no btn perfil

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,40 @@ import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function Header({ pageName }) {
+function Header({ pageName, history }) {
   return (
     <header>
-      <h1 data-testid="page-title">
+      <h1
+        data-testid="page-title"
+        >
         {pageName}
         {' '}
       </h1>
+
       <img
         src={ profileIcon }
         data-testid="profile-top-btn"
         alt="Ícone de Perfil"
       />
+
       <img
         src={ searchIcon }
         data-testid="search-top-btn"
         alt="Ícone de Perfil de busca"
       />
+
+      <button
+        type="button"
+        onClick={ () => { history.push('/profile'); } }
+      ></button>
     </header>
   );
 }
 Header.propTypes = {
-  pageName: PropTypes.string.isRequired,
-};
+  pageName: PropTypes.string,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  })
+}.isRequired;
 
 export default Header;
