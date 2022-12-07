c6b1d1ec2bd1907bd33b476187c47b6ef79d456a
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2022-12-07 13:55:46 -0300

    req 29 e 28

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
@@ -11,20 +11,20 @@ import Recipes from './pages/Recipes';
 function App() {
   return (
 
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/drinks" component={ Recipes } />
-        <Route exact path="/meals" component={ Recipes } />
-        <Route path="/meals/:id" component={ RecipeDetails } />
-        <Route path="/drinks/:id" component={ RecipeDetails } />
-        <Route path="/meals/:id/in-progress" component={ Login } />
-        <Route path="/drinks/:id/in-progress" component={ Login } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/done_recipes" component={ DoneRecipes } />
-        <Route exact path="/favorite_recipes" component={ FavoriteRecipes } />
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/drinks" component={ Recipes } />
+      <Route exact path="/meals" component={ Recipes } />
+      <Route path="/meals/:id" component={ RecipeDetails } />
+      <Route path="/drinks/:id" component={ RecipeDetails } />
+      <Route path="/meals/:id/in-progress" component={ Login } />
+      <Route path="/drinks/:id/in-progress" component={ Login } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done_recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite_recipes" component={ FavoriteRecipes } />
+    </Switch>
+    // </BrowserRouter>
   );
 }
 
--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -1,5 +1,4 @@
 import { screen, act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import { renderWithRouterAndRedux } from './RenderWithL';
@@ -38,13 +37,25 @@ afterAll(() => {
 
 it('testa o arquivo RecipesDetails', async () => {
   const { history } = renderWithRouterAndRedux(<App />);
+
   act(() => {
-    history.push({ pathname: '/meals/52977' });
+    history.push('/meals/52977');
   });
   const image = await screen.findByRole('img');
+  expect(image).toBeInTheDocument();
+
+  const nome = await screen.findByTestId('recipe-title');
+  expect(nome).toBeInTheDocument();
 });
 
 it('testa o arquivo RecipesDetails', async () => {
-  renderWithRouterAndRedux(<RecipesDetails />, { initialEntries: ['drinks/13501'] });
+  const { history } = renderWithRouterAndRedux(<App />);
+
+  act(() => {
+    history.push('/drinks/13501');
+  });
   const image = await screen.findByRole('img');
+  expect(image).toBeInTheDocument();
+  const nome = await screen.findByTestId('recipe-title');
+  expect(nome).toBeInTheDocument();
 });
